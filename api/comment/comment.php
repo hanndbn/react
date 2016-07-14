aaf2288d7ca43b6b358724b1b6f614119e43f83a
hanndbn <han.nguyendinh45@gmail.com>
2016-07-14 15:53:14 +0700

    create example 2

---
+++ api/comment/comment.php
@@ -0,0 +1,6 @@
+<?php
+
+    $json = '[{"id": "1", "author": "Pete Hunt", "text": "This is one comment"},{"id": "2", "author": "Jordan Walke", "text": "This is *another* commen"}]';
+    $return["json"] = $json;
+    return $json;
+
--- src/example1.js
+++ src/example1.js
@@ -15,19 +15,80 @@ var CommentList = React.createClass({
     }
 });
 var CommentForm = React.createClass({
+    getInitialState: function(){
+        return {author: '', text:''}
+    },
+    handleAuthorChange: function(e){
+        this.setState({author: e.target.value})
+    },
+    handleTextChange: function(e){
+        this.setState({text: e.target.value})
+    },
+    handleSubmit: function (e) {
+        e.preventDefault();
+        var author = this.state.author.trim();
+        var text = this.state.text.trim();
+        if(!text || !author){
+            return;
+        }
+        this.props.onCommentSubmit({author:author, text:text});
+        this.setState({author:'',text:''});
+    },
     render: function () {
+        var divstyle = {
+            display: 'block'
+        };
         return (
-            React.createElement('div',{className:'commentForm'},"Hello I am comment Form")
+            <form className="commentForm" style={divstyle} onSubmit={this.handleSubmit}>
+                <input type="text" placeholder="Your name" style={{display:'block'}} value={this.state.author} onChange={this.handleAuthorChange}/>
+                <input type="text" placeholder="Say something" style={{display:'block'}} value={this.state.text} onChange={this.handleTextChange}/>
+                <input type="submit" value="Post"/>
+            </form>
         )
     }
 });
 var CommentBox = React.createClass({
+    loadCommentFromServer: function() {
+        $.ajax({
+            url: this.props.url,
+            datatype: 'json',
+            cache: false,
+            success: function (data) {
+                this.setState({data: data})
+            }.bind(this),
+            error: function (xhr, status, err) {
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        });
+    },
+    handleCommentSubmit: function(comment){
+        $.ajax({
+            url: this.props.url,
+            datatype: 'json',
+            type: 'POST',
+            data: comment,
+            success: function(data){
+                this.setState({data: data});
+            }.bind(this),
+            error: function (xhr, status, err) {
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        })
+
+    },
+    getInitialState: function(){
+      return {data: []};
+    },
+    componentDidMount: function(){
+        this.loadCommentFromServer();
+        setInterval(this.loadCommentFromServer, this.props.pollInterval);
+    },
     render: function () {
         return (
             <div className="commentBox">
                 <h1>Comments</h1>
-                <CommentList data={this.props.data}/>
-                <CommentForm />
+                <CommentList data={this.state.data}/>
+                <CommentForm onCommentSubmit={this.handleCommentSubmit}/>
                </div>
         );
     }
@@ -58,6 +119,6 @@ var data = [
 ];
 
 ReactDOM.render(
-    <CommentBox data={data}/>,
+    <CommentBox url="api/comment/comment.json" poolInterval = {2000}/>,
     document.getElementById('content')
 );
